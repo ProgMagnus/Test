1b007dbb57ee08b4343f188b1e7654daed4a707d
Cat's code <teamofpandas@gmail.com>
2021-07-04 12:30:45 +0300

    add new functions

--- newfile.py
+++ newfile.py
@@ -1,27 +1,44 @@
 import random
 import time
+import sys
+import re
 
-print('Hola! find my num\nbetween 1 - 2')
+print('Hola! Guess the number\nfrom 1 to 20')
 
 class RunGame():
 	game = True
+	player_score = 0
+	pc_score = 0
 
 def run():
-	qt = 0
-	pn = random.randint(1, 20)
-	while qt < 6:
-		ur = int(input())
-		qt += 1
-		
-		if ur > pn:
-			print('ur >')	
-		if ur < pn:
-			print('ut <')			
-		if ur == pn:
-			print('win') 
-			break	
-	if ur != pn:
-		print('u lose')
+	quantity = 0
+	pc_number = random.randint(1, 20)
+	while quantity < 6:
+		player_number = int(input())
+		quantity += 1
+		if player_number > 20 and player_number < 99:
+			print('I said it was a number between 1 and 20.')
+		if player_number > pc_number and player_number < 21:
+			time.sleep(0.5)
+			print('Your number is greater than mine!')	
+		elif player_number < pc_number:
+			time.sleep(0.5)
+			print('Your number is less than mine')			
+		elif player_number == pc_number: 
+			break
+		elif player_number == 99:
+			time.sleep(0.5)
+			print('Bye!')
+			time.sleep(0.5)
+			sys.exit()	
+	if player_number == pc_number:
+		g.player_score += 1
+		time.sleep(0.5)
+		print(f"You won!\nResults:\nwin: {g.player_score}, lose: {g.pc_score} \nIf you want to exit type '99'")
+	elif player_number != pc_number:
+		g.pc_score += 1
+		time.sleep(0.5)
+		print(f"You lose!\nResults:\nwin: {g.player_score}, lose: {g.pc_score} \nIf you want to exit type '99'")
 		
 g = RunGame()
 while g.game == True:
